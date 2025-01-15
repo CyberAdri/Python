cea9df178f313c5353cc4871a52f9dd83a1ee47b
Letícia Adriana <letadri61@gmail.com>
2025-01-15 17:34:30 -0300

    Imprimir
    Aqui aprendi o primeiro comando em Python, como imprimir e adicionar comentários.

---
+++ teste1.py
@@ -0,0 +1,19 @@
+"""
+DocString
+O sistema lê o que está aqui mas não considera.
+
+"""
+
+'''
+O sistema lê o que está aqui mas não considera.
+
+'''
+
+print('hello, world')
+#para inserir comentários.
+print('Meu nome é teste 1')
+
+
+print(157)
+print(287)
+#como infomar números
