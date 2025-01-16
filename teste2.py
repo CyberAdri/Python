bec2f1d8bed07fb9648ef8f5fb6a384ae35786c2
Letícia Adriana <letadri61@gmail.com>
2025-01-15 22:41:17 -0300

    Print aula 2
    Nesta segunda aula, aprendi a como utilizar, diferente da linguagem c, um print como função.

---
+++ teste2.py
@@ -0,0 +1,14 @@
+# se você estiver em um computador antigo será necessário utilizar o \n e \r= CRLF para pular a linha.
+# o "sep" = ao printar as informações, ele utilizar o argumento mencionado.
+# end significa como você gostaria de costumizar o fim do print.
+
+print(12, 33, 66, sep=" - ", end= '' )
+print(55, 44, 22, sep=" - " )
+print(159, 63, 77, sep=" - " )
+
+#para destacar um texto, você poderá utilizar: \" ou essa sequência abaixo.
+print('amorroz "abroba"' )
+print("amorroz \"abroba" )
+
+print(r"amorroz \"abroba\"")
+
